b3b0f56c7f2d61222214986e34e90f764f552ace
Arjun <arjunin051@gmail.com>
2026-08-27 20:58:36 +0530

    Modified Transactions file

--- src/Transactions.js
+++ src/Transactions.js
@@ -1,23 +1,45 @@
 import React from 'react'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './Transactions.css'
+import { addTransactionList, getTransaction } from './services/userservice'
 
-const Transactions = ({amount,setAmount}) => {
+const Transactions = () => {
     const[showForm,setShowForm] = useState(false)
-    const[transactoinType,setTransactionType] = useState("expense")
-    const[Category,setCategory] = useState("food")
+    const[transactionType,setTransactionType] = useState("expense")
+    const[amount,setAmount] = useState("")
+    const[category,setCategory] = useState("food")
+    const[date,setDate] = useState(new Date().toISOString().split("T")[0])
     const[payment,setPayment] = useState("upi")
-    const[decription,setDescription] = useState("")
+    const[description,setDescription] = useState("")
     const[notes,setNotes] = useState("")
 
-    const today = new Date()
-    const dateString = today.toLocaleDateString();
+    const[transactions,setTransactions] = useState([])
 
+    const handleTransaction = async () =>{
+        const id = transactions.length ? transactions[transactions.length - 1].id + 1 : 1
+        const addNewTransaction = {
+          id,
+          type : transactionType,
+          amount,
+          category,
+          date,
+          paymentMethod: payment,
+          description,
+          notes
+        }
 
+        const response = await addTransactionList(addNewTransaction)
+        const transactionList = prev => ([...prev , response.data])
+        setTransactions(transactionList)
 
-  /* const transactionForm = () =>{
+    }
 
-  } */
+    useEffect(() => {fetchTransaction()},[])
+
+    const fetchTransaction = async () => {
+        const response = await getTransaction()
+        setTransactions(response.data)
+    }
   return (
     <div>
         <h1>Transactions</h1>
@@ -38,14 +60,14 @@ const Transactions = ({amount,setAmount}) => {
               </button>
 
             
-        <form className='transaction-form' action="">
+        <form className='transaction-form' onSubmit={handleTransaction}>
           <div className='type-toggle'>
             <label>
               <input 
                 type="radio" 
                 name = "transaction-type" 
                 value="expense"
-                checked = {transactoinType === "expense"}
+                checked = {transactionType === "expense"}
                 onChange={(event) => (setTransactionType(event.target.value))}
                 defaultChecked
               />
@@ -57,7 +79,7 @@ const Transactions = ({amount,setAmount}) => {
                type="radio"
                name = "transaction-type"
                value="income"
-               checked = {transactoinType === "income"}
+               checked = {transactionType === "income"}
                onChange={(event) => setTransactionType(event.target.value)}
               />
               <span>Income</span>
@@ -72,6 +94,7 @@ const Transactions = ({amount,setAmount}) => {
               placeholder='0.00'
               value={amount}
               onChange={(event) => setAmount(event.target.value)}
+              required
  
             />
           </label>
@@ -79,7 +102,7 @@ const Transactions = ({amount,setAmount}) => {
           <label className='form-field'>
             Category
             <select name="expense-options" id="expense-options"
-              value={Category} onChange={(event) => setCategory(event.target.value)} 
+              value={category} onChange={(event) => setCategory(event.target.value)} 
             >
               <option value="food">Food</option>
               <option value="rent">Rent</option>
@@ -90,8 +113,11 @@ const Transactions = ({amount,setAmount}) => {
 
           <label className='form-field'>
             Date
-            {/* <input type="date" /> */}
-            {dateString}
+            <input 
+              type="date" 
+              value={date}
+              onChange={(event) => setDate(event.target.value)}
+            />
           </label>
 
           <label className='form-field'>
@@ -139,6 +165,36 @@ const Transactions = ({amount,setAmount}) => {
         </div>
         </div>
         )}
+        <table>
+          <thead>
+            <tr>
+              <th>Type</th>
+              <th>Amount</th>
+              <th>Category</th>
+              <th>Date</th>
+              <th>Payement Method</th>
+              <th>Description</th>
+              <th>Notes</th>
+            </tr>
+          </thead>
+
+          <tbody>
+            {transactions.map((transaction) => (
+              <tr key={transaction.id}>
+                <td>{transaction.type}</td>
+                <td>{transaction.amount}</td>
+                <td>{transaction.category}</td>
+                <td>{transaction.date}</td>
+                <td>{transaction.paymentMethod}</td>
+                <td>{transaction.description}</td>
+                <td>{transaction.notes}</td>
+              </tr>
+
+            ))
+
+            }
+          </tbody>
+        </table>
         
     </div>
   )
